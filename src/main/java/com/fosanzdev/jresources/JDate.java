0350908524dff243765b154bc4f0432cbf472283
Esteban Sánchez <108540114+FosanzDev@users.noreply.github.com>
2023-03-10 09:46:50 +0100

    improved dateToPattern: now it can work with multiple separators and default separators

--- src/main/java/com/fosanzdev/jresources/JDate.java
+++ src/main/java/com/fosanzdev/jresources/JDate.java
@@ -26,7 +26,7 @@ public class JDate {
                 //Requests the date
                 res = JRequest.requestString(msj);
                 //Creates the pattern from the format
-                String pat = dateToPattern(format, "/");
+                String pat = dateToPattern(format);
                 //Compile and compare with the date
                 Pattern pattern = Pattern.compile(pat);
                 Matcher m = pattern.matcher(res);
@@ -63,16 +63,39 @@ public class JDate {
 
     /**
      * Transforms a date format to a regular expression
-     * The returned string has the same separators as the format
+     * The separator used is the char given in defaultSeparator
      *
      * @param date      String with the date format
-     * @param separator String with the separator
+     * @param separators char array with the separators
+     * @param defaultSeparator char with the default separator
      * @return String with the regular expression
      */
-    public static String dateToPattern(String date, String separator) {
-        String[] dateParts = date.split(separator);
-        return String.format("\\d{%d}%s\\d{%d}%s\\d{%d}", dateParts[0].length(), separator, dateParts[1].length(), separator, dateParts[2].length());
+    public static String dateToPattern(String date, char[] separators, char defaultSeparator) {
+        String[] dateParts = date.split("[" + new String(separators) + "]");
+        return String.format("\\d{%d}%s\\d{%d}%s\\d{%d}", dateParts[0].length(), defaultSeparator, dateParts[1].length(), defaultSeparator, dateParts[2].length());
     }
 
+    /**
+     * Transforms a date format to a regular expression
+     * The default separator is /
+     *
+     * @param date String with the date format
+     * @param separators char array with the separators
+     * @return String with the regular expression
+     */
+    public static String dateToPattern(String date, char[] separators) {
+        return dateToPattern(date, separators, '/');
+    }
+
+    /**
+     * Transforms a date format to a regular expression
+     * The default separators are '/', '-', '.'
+     *
+     * @param date String with the date format
+     * @return String with the regular expression
+     */
+    public static String dateToPattern(String date){
+        return dateToPattern(date, new char[]{'/', '-', '.'}, '/');
+    }
 
 }
