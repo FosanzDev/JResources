c7b8530d636df36d823008460fa61c72b7249807
Esteban Sánchez <108540114+FosanzDev@users.noreply.github.com>
2023-03-10 11:03:02 +0100

    changed DeployMenu
    DeployMenu now gets the footer from the last option and receives a boolean indicating whether if the footer is active or not

--- src/main/java/com/fosanzdev/jresources/JDeploy.java
+++ src/main/java/com/fosanzdev/jresources/JDeploy.java
@@ -3,63 +3,64 @@ package com.fosanzdev.jresources;
 public class JDeploy {
 
     /**
-            * Prints a menu with personalized title, footer and initial sequence per element given.
-            * <p>
+     * Prints a menu with personalized title, footer and initial sequence per element given.
+     * <p>
      * If the initial sequence includes a {@code "."}, an autoincremental index will be added before the {@code "."}.
-            * <p>
+     * <p>
      * Footer lenght equals the title columns. If footer ==  {@code " "}, footer won't be printed.
-            * <p>
+     * <p>
      * Examples:
-            *  <blockquote><pre>
-     *deployMenu("Example menu", " ", "--> ", "Opt1", "Opt2");
+     * <blockquote><pre>
+     * deployMenu("Example menu", "--> ", false, "Opt1", "Opt2");
      *  //******************
-             *  //** Example menu **
-             *  //******************
-             *  // --> Opt1
-             *  // --> Opt2
-             *
-             *deployMenu("Example menu with id", "Footer!", "--> . ", "Opt1", "Opt2");
+     *  //** Example menu **
+     *  //******************
+     *  // --> Opt1
+     *  // --> Opt2
+     *
+     * deployMenu("Example menu with id", "--> . ", true, "Opt1", "Opt2", "Footer!");
      *  //**************************
-             *  //** Example menu with id **
-             *  //**************************
-             *  // --> 1. Opt1
-             *  // --> 2. Opt2
-             *  //--------------------------
-             *  // Footer!
-             *  </pre></blockquote>
-            *
-            * @param title String with the title
-     * @param footer String with the footer
-     * @param init String with the starting sequence.
-            * @param args Receives an undefinied number of Strings or a String array ({@code String[]})
-            */
-    public static void deployMenu(String title, String footer, String init, String... args){
+     *  //** Example menu with id **
+     *  //**************************
+     *  // --> 1. Opt1
+     *  // --> 2. Opt2
+     *  //--------------------------
+     *  // Footer!
+     *  </pre></blockquote>
+     *
+     * @param title  String with the title
+     * @param init   String with the starting sequence.
+     * @param footer boolean specifying if the footer will be printed or not.
+     *               If {@code true}, the last element of the array will be the footer.
+     *               If {@code false}, the last element of the array will be the last option.
+     * @param args   Receives an undefinied number of Strings or a String array ({@code String[]})
+     */
+    public static void deployMenu(String title, String init, boolean footer, String... args) {
         int idx = 1;
-        String titleMod = new StringBuilder("*".repeat(title.length() + 6)
+        String titleMod = "*".repeat(title.length() + 6)
                 + "\n** " + title + " **\n"
-                + "*".repeat(title.length() + 6)).toString();
+                + "*".repeat(title.length() + 6);
 
         System.out.println(titleMod);
 
-        if (init.contains(".")){
+        if (init.contains(".")) {
 
-            for (String arg : args){
+            for (int i = 0; i < args.length - 1; i++) {
                 String initMod = init.replace(".", idx + ".");
-                System.out.println(initMod + arg);
+                System.out.println(initMod + args[i]);
                 idx++;
             }
-        }
-
-        else{
-            for (String arg : args){
-                System.out.println(init + arg);
+        } else {
+            for (int i = 0; i < args.length - 1; i++) {
+                System.out.println(init + args[i]);
             }
         }
 
-        if(footer.equals(" ") || footer.equals(""));
-        else{
+        if (footer) {
             System.out.println("-".repeat(title.length() + 6));
-            System.out.println(footer);
+            System.out.println(args[args.length - 1]);
+        } else {
+            System.out.println(args[args.length - 1]);
         }
     }
 
